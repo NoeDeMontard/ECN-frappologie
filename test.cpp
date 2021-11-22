4c3d9fe7969d79c0bad747ac9e6177cc4f93f599
Clément <94597519+clementndt@users.noreply.github.com>
2021-11-22 10:43:17 +0100

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string name;
+
+  cout << "Test fonction getline";
+  getline (cin,test);
+  cout <<  test << endline;
+
+  return 0;
+}
+
+//
+
