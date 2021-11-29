723a4f9e86865863612f81a2eed9503401e89f95
Noe de Montard <Noe.de-Montard@eleves.ec-nantes.fr>
2021-11-29 09:44:47 +0100

    add time handling

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <conio.h>
 #include <iostream>
+#include <chrono>
+#include <vector>
 #include "constantes.h"
 #include "affichage.h"
 using namespace std;
@@ -10,9 +12,20 @@ int main()
 {
     int c;
     int encore = 1;
+    // Time mesurement
+    vector<chrono::time_point<chrono::high_resolution_clock>> times;
     while (encore == 1) {
         c = _getch();
+        times.push_back(chrono::high_resolution_clock::now());
         affichage(c, encore);
+    }
+    // Compture the times and key combinaisons
+    chrono::time_point<chrono::high_resolution_clock> tempsTouchePrecedente = times[0];
+    for (int i = 0; i< times.size(); i++){
+        chrono::time_point<chrono::high_resolution_clock> tempsToucheActuelle = times[i];
+        chrono::duration_cast<chrono::microseconds>(tempsToucheActuelle-tempsTouchePrecedente).count(); // nanoseconds, microseconds, milliseconds
+        tempsTouchePrecedente = tempsToucheActuelle;
+        // TODO Processing the duration - key pair combinaison
     }
     return 0;
 }
