eeb4a7ea8226d315b8ca99aa5f33d8be5b17259a
Noe de Montard <Noe.de-Montard@eleves.ec-nantes.fr>
2022-01-21 09:54:54 +0100

    define constants at the begining of the file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h> // Used by CreateDirectory
 #include <math.h>
-#include <cstdio>    // Used by remove in c++<17
+#include <cstdio> // Used by remove in c++<17
 #include <filesystem>
 #include <iostream>
 #include <string>
@@ -10,58 +10,65 @@
 #include "language.h"
 #include "lib.h"
 
+#define PASSWORDFILENAME "passwordFile"
+#define PASSWORDFOLDERPATH "passwords"
+
 using namespace std;
 // void CreateDirectory(const char*, int*);
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    const string passwordFileName = "passwordFile";
-    const string passwordFolderPath = "passwords";
+    const string passwordFileName = PASSWORDFILENAME;
+    const string passwordFolderPath = PASSWORDFOLDERPATH;
     bool accessGranted = true;
     // language defined in langiage.h and language.cpp
-    if (argc == 2){
+    if (argc == 2)
+    {
         cout << argv[0] << " " << argv[1] << endl;
-        if (_stricmp( argv[1], "fr" ) == 0){
+        if (_stricmp(argv[1], "fr") == 0)
+        {
             language = &french;
-        } else if (_stricmp( argv[1], "en" ) == 0){
+        }
+        else if (_stricmp(argv[1], "en") == 0)
+        {
             language = &english;
         }
     }
-
 
     // Create the passwordFolder if it doesn't exists
     CreateDirectory(passwordFolderPath.c_str(), NULL);
     //filesystem::create_directory(passwordFolderPath);// If c++17, more portable
     // Both fail if the passwordFolderPath use a path with a missing parent directory
 
-
     cout << language->welcome << endl;
     cout << language->developpers << endl;
-    cout << language->registrationOrAuthentification << endl;    string choice;
+    cout << language->registrationOrAuthentification << endl;
+    string choice;
     cin >> choice;
     string user;
     cout << language->usernameInput << endl;
     cin >> user;
     std::size_t found = user.find_first_of("/\\");
-    while (found!=string::npos)
+    while (found != string::npos)
     {
-        user[found]='_';
-        found=user.find_first_of("/\\",found+1);
+        user[found] = '_';
+        found = user.find_first_of("/\\", found + 1);
     }
-    if (*find(language->registration.begin(), language->registration.end(), choice) == choice) {
+    if (*find(language->registration.begin(), language->registration.end(), choice) == choice)
+    {
         // THE PASSWORD FILE INITIALISATION METHOD
         registerPasswordTimes(passwordFolderPath + "/" + user + "." + passwordFileName);
     }
-    else {
+    else
+    {
         // THE PASSWORD CHECKING METHOD
         accessGranted = testPasswordTimes(passwordFolderPath + "/" + user + "." + passwordFileName);
 
-        if (VERBOSITY>=1){
+        if (VERBOSITY >= 1)
+        {
             cout << (accessGranted ? language->success : language->failure) << endl;
         }
     }
 
-
-    return (accessGranted?0:3);
+    return (accessGranted ? 0 : 3);
 }
